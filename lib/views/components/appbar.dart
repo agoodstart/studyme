71d76d39c407ad243f07061eb37c4cdaf6bb5e16
agoodstart <jvalkenhoff@outlook.com>
2020-04-05 16:48:28 +0200

    Adding login screen, bronnen

---
+++ lib/views/components/appbar.dart
@@ -0,0 +1,7 @@
+// import 'package:flutter/material.dart';
+
+// _homeAppBar(String title) {
+//   return Scaffold(
+//     appBar: AppBar(title: Text(title)),
+//   );
+// }
---
+++ lib/views/template.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+// import 'components/appbar.dart';
+// import 'package:flutter_webrtc/webrtc.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Widget _buildColumn() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        RaisedButton(
+          onPressed: () {},
+          child: const Text(
+            'Sign in',
+            style: TextStyle(fontSize: 20)
+            ),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+        ),
+        const SizedBox(height: 30),
+        RaisedButton(
+          onPressed: () {},
+          child: const Text(
+            'Sign up',
+            style: TextStyle(fontSize: 20)
+          ),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          constraints: BoxConstraints.expand(
+            height: 200,
+            width: 200),
+          child: _buildColumn(),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(25),
+            color: Colors.amber[600],
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 5.0,
+                spreadRadius: 1.0,
+                offset: Offset(
+                  0.0,
+                  2.5
+                )
+              )
+            ]
+          ),
+        )
+      ),// This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
